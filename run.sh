c1fa079154851d3016f0a4f222656d9f0572fcb7
atlet99 <joha.shadibekov@gmail.com>
2024-03-22 01:20:28 +0500

    [ADD] to set the current timezone from the host without manual specification and fix the hostname for the redirect process;

--- run.sh
+++ run.sh
@@ -3,6 +3,12 @@
 # Get hostname from current host
 HOSTNAME=$(hostname)
 
+# Define timedatectl tool
+TIMEDATE_CTL="$PATH/timedatectl"
+
+# Get current timezone
+CURRENT_TIMEZONE=$("$TIMEDATE_CTL" | grep "Time zone" | awk '{print $3}')
+
 # Define path to docker-compose
 DOCKER_COMPOSE_FILE="docker-compose.yml"
 
@@ -14,6 +20,7 @@ DOCKER_COMPOSE="$PATH/docker-compose"
 
 # Replace hostname value in docker-compose.yml
 sed "s/example.com/$HOSTNAME/g" "$DOCKER_COMPOSE_FILE" > "$TEMP_FILE"
+sed "s/Asia\/Tashkent/$CURRENT_TIMEZONE/g" "$DOCKER_COMPOSE_FILE" >> "$TEMP_FILE"
 
 # Run changed docker-compose with updated hostname
 sudo "$DOCKER_COMPOSE" -f "$TEMP_FILE" up -d --build
