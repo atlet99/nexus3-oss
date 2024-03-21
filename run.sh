a10cc0e30994a3886f6b16207d97c5f60911e1e2
atlet99 <joha.shadibekov@gmail.com>
2024-03-22 00:54:50 +0500

    [FIX] fix temp-file name instead mktemp and fix docker-compose path of binary;

--- run.sh
+++ run.sh
@@ -7,13 +7,13 @@ HOSTNAME=$(hostname)
 DOCKER_COMPOSE_FILE="docker-compose.yml"
 
 # Define temp-file
-TEMP_FILE=$(mktemp)
+TEMP_FILE=temp.yml
 
 # Define absolute path to docker-compose binary file
-DOCKER_COMPOSE=/usr/local/bin/docker-compose
+DOCKER_COMPOSE="$PATH/docker-compose"
 
 # Replace hostname value in docker-compose.yml
-sed -i "s/example.com/$HOSTNAME/g" "$DOCKER_COMPOSE_FILE" > "$TEMP_FILE"
+sed "s/example.com/$HOSTNAME/g" "$DOCKER_COMPOSE_FILE" > "$TEMP_FILE"
 
 # Run changed docker-compose with updated hostname
 sudo "$DOCKER_COMPOSE" -f "$TEMP_FILE" up -d --build
